0c034341787999825932762b523b57488c98cf22
Luiz Felipe G. Pereira <luiz.felipe@hey.com>
2024-01-24 12:56:40 -0300

    Add variable

--- main.tf
+++ main.tf
@@ -7,9 +7,15 @@ terraform {
   }
 }
 
-provider "random" {}
+variable "pet_name_length" {
+  description = "The length of the random pet name"
+  type        = number
+  default     = 2
+}
 
-resource "random_pet" "name" {}
+resource "random_pet" "name" {
+  length    = var.pet_name_length
+}
 
 output "random_pet_name" {
   value = random_pet.name.id
